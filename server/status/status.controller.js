4e4b7107402eb003836f511f5602c63036e9f70d
Vladyslav Golyachenko <vladyslav.golyachenko@icanbwell.com>
2021-10-28 20:23:02 +0300

    convert data to fixed

--- server/status/status.controller.js
+++ server/status/status.controller.js
@@ -1,9 +1,17 @@
 const Status = require('./status.model')
+const { get } = require('lodash')
 
 module.exports = {
   createStatusRecord: async (req, res, next) => {
     try {
-      const result = await Status.create(req.body)
+      let obj
+      Object.keys(req.body).forEach(key => {
+        obj = {
+          ...obj,
+          [key]: get(req.body, key, 0).toFixed(2),
+        }
+      })
+      const result = await Status.create(obj)
       return res.status(200).json(result)
     } catch (error) {
       next(error)
