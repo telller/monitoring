2b7db139561e6562f317da5d90889668423ed241
Vladyslav Golyachenko <vladyslav.golyachenko@icanbwell.com>
2021-10-28 21:39:14 +0300

    upd

--- server/status/status.controller.js
+++ server/status/status.controller.js
@@ -4,13 +4,16 @@ const { get } = require('lodash')
 module.exports = {
   createStatusRecord: async (req, res, next) => {
     try {
-      let obj
+      let obj = { ...req.body }
       Object.keys(req.body).forEach(key => {
-        obj = {
-          ...obj,
-          [key]: get(req.body, key, 0).toFixed(2),
+        if (key !== 'dateTime') {
+          obj = {
+            ...obj,
+            [key]: +get(req.body, key, 0).toFixed(2),
+          }
         }
       })
+      console.log({ obj, body: req.body })
       const result = await Status.create(obj)
       return res.status(200).json(result)
     } catch (error) {
